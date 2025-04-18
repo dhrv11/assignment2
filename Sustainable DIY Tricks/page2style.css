aa6a785f6a82d70c85312963f2f321ad23d7cb83
dhrv11 <132676480+dhrv11@users.noreply.github.com>
2025-04-18 09:45:53 +0530

    Add files via upload

---
+++ Sustainable DIY Tricks/page2style.css
@@ -0,0 +1,33 @@
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body, html {
+  height: 100%;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.background-image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  display: block;
+}
+
+.next-arrow {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  font-size: 36px;
+  color: black;
+  cursor: pointer;
+}
